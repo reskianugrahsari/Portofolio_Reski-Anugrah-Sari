3d30a4d66d47d11addc1e7d0f64252ef0904b0a1
Reski Anugrah Sari <reskianugrahsari@gmail.com>
2026-09-15 08:15:28 +0800

    Update hero name

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -53,7 +53,7 @@ const Hero: React.FC = () => {
           </div>
 
           <h1 className="text-5xl md:text-6xl lg:text-7xl font-black text-white mb-2 leading-[1] tracking-tighter">
-            Sari
+           Reski Anugrah Sari
           </h1>
 
           <p className="text-xl md:text-2xl text-[#f0eaff] mb-6">
